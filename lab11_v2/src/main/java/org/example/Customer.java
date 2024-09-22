c185b4a451675a7fd70800abf6c644ce43432bdb
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-09-22 12:58:26 +0300

    fix

---
+++ lab11_v2/src/main/java/org/example/Customer.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "\n[Покупатель: " + this.getName() + "]\n";
+    }
+}
